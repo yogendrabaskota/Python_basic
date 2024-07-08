7d5f10f757832a7898c6f41bd9211311ac512062
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-07-08 21:49:21 +0545

    variables

---
+++ variables.py
@@ -0,0 +1,10 @@
+#variable declaration
+x = 3 #int
+y = 10 #int
+z = x + y 
+print("the sum of x and y is ", z)
+
+#type conversion
+x = float(x) #convert x to float
+print(type(x))  #output: <class 'float'>
+print(x)  #output is 3.0 because int 3 is converted to float
