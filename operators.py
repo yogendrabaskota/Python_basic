94a75dec935a1803745d9d9d3a43ad636ba2b1bb
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-07-09 01:37:43 +0545

    operators

--- operators.py
+++ operators.py
@@ -8,3 +8,31 @@ print("multiplication between", x, "and", y, "is", x*y)
 print(x ** y)  # output will be very largeeee
 print(x // y)  # Floor division: 5
 
+#COMPARISON OPERATOR
+print(x == y) #false
+print(x > y) 
+print(x < y)
+print(x != y)
+print(x >= y)
+print(x <= y)
+
+#LOGICAL OPERATOR
+a = True
+b = False
+print(a and b) #false
+print(a or b) #true
+print(not b) #true
+
+
+#ASSIGNMENT OPERATOR
+p = 5
+print(p) # p=5
+p+=5
+print(p)  #p=p+5=5+5=10
+p*=6
+print(p) #p=p*6=10*6=60
+
+
+#BITWISE OPERATOR
+
+
