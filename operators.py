8fb2241917ec1901cee3d7a40483476e035ff2dd
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-07-08 22:05:10 +0545

    operators

---
+++ operators.py
@@ -0,0 +1,10 @@
+x = 100
+y = 20 
+
+print("sum of x and y is ", x+y)
+print("Differencve is ", x-y)
+print("division is ", x/y)
+print("multiplication between", x, "and", y, "is", x*y)
+print(x ** y)  # output will be very largeeee
+print(x // y)  # Floor division: 5
+
