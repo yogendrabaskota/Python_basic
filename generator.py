3b39db6af2806cfa23d5e299f781ab60d4ee38d6
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-07-12 20:56:43 +0545

    generator

--- generator.py
+++ generator.py
@@ -7,5 +7,5 @@ for i in countdown(5):
     print(i)
 
 
-# A Generator in Python is a function that returns an iterator using the Yield keyword.
+# A Generator in Python is a function that returns an iterator using the Yield keyword. 
 # A generator function in Python is defined like a normal function, but whenever it needs to generate a value, it does so with the yield keyword rather than return.
